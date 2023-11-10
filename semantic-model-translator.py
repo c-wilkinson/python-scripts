4debc8ff4fef6894648ad1bbdb766f20aca6666d
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2023-11-10 15:00:40 +0000

    Add progress and rate limits

--- semantic-model-translator.py
+++ semantic-model-translator.py
@@ -3,6 +3,8 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 from xml.etree import ElementTree as ET
+import time
+from tqdm import tqdm
 
 # Function to remove namespace in the passed document in place.
 def remove_namespace(doc):
@@ -10,6 +12,7 @@ def remove_namespace(doc):
         if '}' in elem.tag:
             elem.tag = elem.tag.split('}', 1)[1]  # Removes namespace
 
+# Function to scrape the Google translate mobile website
 def scrape_google_translate(text, source_language, target_language):
     # Google Translate URL
     url = "https://translate.google.com/m"
@@ -22,8 +25,16 @@ def scrape_google_translate(text, source_language, target_language):
         'tl': target_language,
     }
 
-    # Make the request to Google Translate
-    response = requests.get(url, params=payload)
+    # Make the request to Google Translate with rate limiting
+    while True:
+        response = requests.get(url, params=payload)
+        if response.status_code == 200:
+            break
+        elif response.status_code == 429:
+            print("Rate limit exceeded. Waiting for 5 seconds...")
+            time.sleep(5)
+        else:
+            response.raise_for_status()
 
     # Parse the response with BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -40,12 +51,17 @@ def scrape_google_translate(text, source_language, target_language):
     return translated_text
 
 # Function to process an individual file
-def process_file(file_path):
+def process_file(file_path, file_progress_bar):
+    # Parse the XML file
     tree = ET.parse(file_path)
     root = tree.getroot()
     remove_namespace(root)  # Call the function to remove namespaces
 
-    for localization in root.iter('Localization'):
+    # Count the total number of Localization elements in the file
+    total_elements = sum(1 for _ in root.iter('Localization'))
+
+    # Loop through each Localization element
+    for i, localization in enumerate(root.iter('Localization')):
         culture = localization.get('Culture')
         if culture not in ['en-GB', 'en-US']:  # Skip default English localizations
             source_lang = 'en'  # Assuming the source language is English
@@ -59,9 +75,11 @@ def process_file(file_path):
             except Exception as e:  # Catching all other exceptions
                 print(f"General error translating {culture}: {e}")
 
+            # Update element-level progress bar
+            file_progress_bar.update(1)
+
     # Write back to file
     ET.register_namespace('', '')  # Register an empty namespace
-    print(f"Translations added to {file_path}")
     tree.write(file_path, encoding='utf-8', xml_declaration=True)
 
 # Main function to process files within a directory
@@ -70,7 +88,14 @@ def main(root_directory):
         for file in files:
             if file.endswith(".sm"):
                 file_path = os.path.join(subdir, file)
-                process_file(file_path)
+
+                # Initialize file-level progress bar
+                file_progress_bar = tqdm(desc=f"Processing {file}", unit=" element", total=0)
+
+                process_file(file_path, file_progress_bar)
+
+                # Close the file-level progress bar
+                file_progress_bar.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
